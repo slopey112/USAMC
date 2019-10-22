6938e0c5826dc751c992ffa1781789368d7391c7
slopey112 <howard_peng@icloud.com>
2019-10-21 18:16:53 -0700

    finish sn

--- scripts/speed-numbers.js
+++ scripts/speed-numbers.js
@@ -47,6 +47,7 @@ function sleep(ms) {
 let s = 299;
 
 createTable(generateNumbers());
+var timeUsed = 300;
 
 let button = document.getElementById("clear");
 let overlay = document.getElementById("overlay");
@@ -66,10 +67,23 @@ button.onclick = async function () {
 };
 
 function finish() {
+    timeUsed -= s;
     let x = 0;
     let y = 0;
-    document.getElementById("right").removeChild(document.getElementById("overlay"));
+    overlay.parentNode.removeChild(overlay);
     clearInterval(interval);
+    s = 600;
+    let reset = document.getElementsByClassName("control")[1];
+    reset.parentNode.removeChild(reset);
+    interval = setInterval(function () {
+        document.getElementById("timer").innerText = (new Date).clearTime().addSeconds(s).toString('m:ss');
+        if (s === 0) {
+            clearInterval(interval);
+            console.log("done");
+            return;
+        }
+        s--;
+    }, 1000);
     let rows = document.getElementsByTagName("tbody")[0].children;
     for (let i = 0; i < 25; i++) {
         let row = rows[i].children;
@@ -78,34 +92,66 @@ function finish() {
             cell.innerText = "\xa0";
         }
     }
-    document.onkeydown = function(evt) {
+    let input = [];
+    let score = 0;
+    let flag = false;
+    document.onkeydown = async function(evt) {
+        if (flag) return;
         let cell = rows[y].children[x];
         if (!isNaN(evt.key)) {
             cell.innerText = evt.key;
-            if (parseInt(evt.key) === numbers[y][x]) {
-                cell.style.color = "green";
-            } else {
-                cell.style.color = "red";
-            }
             if (x === 19) {
+                let correct = 0;
+                for (let i = 0; i < 20; i++) {
+                    cell = rows[y].children[i];
+                    if (parseInt(cell.innerText) === numbers[y][i]) {
+                        cell.style.color = "green";
+                        correct++;
+                    } else {
+                        cell.style.color = "red";
+                        flag = true;
+                    }
+                }
+                await sleep(1000);
+                if (flag) {
+                    clearInterval(interval);
+                    showScores(score);
+                    console.log("finish");
+                    return;
+                }
                 x = 0;
+                if (y === 24) {
+                    score += correct;
+                    clearInterval(interval);
+                    showScores(score);
+                    return;
+                }
                 y++;
+                score += 20;
             } else {
                 x++;
             }
-        } else if (evt.key === "Backspace") {
-            if (x === 0) {
-                x = 19;
-                y--;
-            } else {
-                x--;
-            }
+        } else if (evt.key === "Backspace" && x !== 0) {
+            x--;
             cell = rows[y].children[x];
             cell.innerText = "";
         }
     }
+
+    document.getElementsByClassName("control")[0].onclick = function () {
+        clearInterval(interval);
+        showScores(score);
+    }
 }
 
+function showScores(pts) {
+    let numbers = document.getElementById("numbers");
+    numbers.parentNode.removeChild(numbers);
+    document.getElementById("score").style.display = "block";
+    let tags = document.getElementsByClassName("n");
+    tags[0].innerText = pts;
+    tags[1].innerText = timeUsed;
+}
 
 let reset = document.getElementsByClassName("control")[1];
 reset.onclick = function() {
